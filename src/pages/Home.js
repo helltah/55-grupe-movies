cf99762d87dd2dcd5c1207462064c1c44482c08b
Vitalij <203612403+helltah@users.noreply.github.com>
2025-07-03 11:25:22 +0300

    footer&main

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,13 @@ import { PageTemplate } from "../templates/PageTemplate.js";
 
 export class PageHome extends PageTemplate {
     main() {
-        return '<main>CONTENT...</main>';
+        return `
+        <main>
+            <div>
+                <div class="col-12">
+                    TEMPLATE PAGE CONTENT         
+                </div>
+            </div>
+        </main>`;
     }
 }
