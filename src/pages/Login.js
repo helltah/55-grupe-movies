7eb23ce317bdcb138b06c1422aefba8c431e489d
Vitalij <203612403+helltah@users.noreply.github.com>
2025-07-07 17:26:19 +0300

    login&register forms

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,17 +1,13 @@
 import { PageTemplate } from "../templates/PageTemplate.js";
+import { loginForm } from "../ui/forms/loginForm.js";
 import { pageTitle } from "../ui/pageTitle.js";
-import { placeholder } from "../ui/placeholder.js";
 
 export class PageLogin extends PageTemplate {
     main() {
         return `
             <main class="container">
                 ${pageTitle('Login title')}
-                ${placeholder('Form', [
-            'email/username',
-            'password',
-            'mygtukas',
-            ])}
-        </main>`;
+                ${loginForm()}    
+            </main>`;
     }
 }
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,19 +1,14 @@
 import { PageTemplate } from "../templates/PageTemplate.js";
+import { registerForm } from "../ui/forms/registerForm.js";
 import { pageTitle } from "../ui/pageTitle.js";
-import { placeholder } from "../ui/placeholder.js";
+
 
 export class PageRegister extends PageTemplate {
     main() {
         return `
             <main class="container">
                 ${pageTitle('Register title')}
-                ${placeholder('Form',[
-                    'username',
-                    'email',
-                    'password',
-                    'sutinku su taisyklemis',
-                    'mygtukas',
-                ])}
-        </main>`;
+                ${registerForm()}
+            </main>`
     }
 }
