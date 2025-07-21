6818b3a4e0e11601e0f6641ded1476a1584393ec
Vitalij <203612403+helltah@users.noreply.github.com>
2025-07-21 11:20:04 +0300

    cateries list

--- src/pages/admin/categories/CategoriesDraft.js
+++ src/pages/admin/categories/CategoriesDraft.js
@@ -1,49 +1,30 @@
+import { getDraftCategories } from "../../../db/getDraftCategories.js";
 import { AdminTemplate } from "../../../templates/AdminTemplate.js";
 import { tableCategories } from "../../../ui/tables/tableCategories.js";
 
-export class PageAdminCategoriesDraft extends AdminTemplate {
-    main() {
-        const data = [
-            {
-                id: 1,
-                title: 'Action',
-                url: 'action',
-                description: 'Lorem ipsum...',
-                isPublished: true,
-                moviesCount: 0,
-            }, {
-                id: 2,
-                title: 'Crime',
-                url: 'crime',
-                description: 'Lorem ipsum...',
-                isPublished: false,
-                moviesCount: 0,
-            }, {
-                id: 3,
-                title: 'Sci-Fi',
-                url: 'sci-fi',
-                description: 'Lorem ipsum...',
-                isPublished: true,
-                moviesCount: 0,
-            }
-        ];
+export  class PageAdminCategoriesDraft extends AdminTemplate {
+    async main() {
+       
+          const data = await getDraftCategories();
 
-        return `
-            <main>
-               <div class="container">
-                    <div class="row">
-                        <div class="col-12">
-                            <h1 class="display-5">Draft categories</h1>
+          
+          
+            return `
+                <main>
+                   <div class="container">
+                        <div class="row">
+                            <div class="col-12">
+                                <h1 class="display-5">Draft categories</h1>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="container">
-                    <div class="row">
-                        <div class="col-12">
-                            ${tableCategories(data)}
-                        </div>
-                    </div>
-                </div>
-            </main>`;
+                    <div class="container">
+                                        <div class="row">
+                                            <div class="col-12">
+                                                ${tableCategories(data)}
+                                            </div>
+                                        </div>
+                                    </div>
+                </main>`;
+        }
     }
-}
