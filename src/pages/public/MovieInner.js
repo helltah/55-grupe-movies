326c667d343b374ea01734f4c4134e8308b83c59
Vitalij <203612403+helltah@users.noreply.github.com>
2025-07-28 11:19:03 +0300

    Movie inner wip

---
+++ src/pages/public/MovieInner.js
@@ -0,0 +1,26 @@
+import { getPublicMovieByURl } from "../../db/public/getMovieByUrl.js";
+import { PageTemplate } from "../../templates/PageTemplate.js";
+import { pageTitle } from "../../ui/pageTitle.js";
+
+export class PageMovieInner extends PageTemplate {
+    constructor(req) {
+        super(req);
+    }
+
+    async main() {
+        const url = this.req.params.movie;
+        const movie = await getPublicMovieByURl(url);
+        
+        if (movie) {
+            return ` 
+                <main>
+                    ${pageTitle(movie.title)}
+                </main>`;
+        }
+
+        return `
+            <main>
+                ${pageTitle(`Movie "${url}" not found`)}
+            </main>`;
+    }
+}
--- src/routes/publicPageRouter.js
+++ src/routes/publicPageRouter.js
@@ -5,12 +5,14 @@ import { PageCategories } from '../pages/public/Categories.js';
 import { PageCategoryInner } from '../pages/public/CategoryInner.js';
 import { PageLogin } from '../pages/public/Login.js';
 import { PageRegister } from '../pages/public/Register.js';
+import { PageMovieInner } from '../pages/public/MovieInner.js';
 
 export const publicPageRouter = express.Router();
 
 publicPageRouter.get('/', async (req, res) => res.send(await new PageHome(req).render()));
 
 publicPageRouter.get('/movies', async (req, res) => res.send(await new PageMovies(req).render()));
+publicPageRouter.get('/movies/:movie', async (req, res) => res.send(await new PageMovieInner(req).render()));
 
 publicPageRouter.get('/categories', async (req, res) => res.send(await new PageCategories(req).render()));
 publicPageRouter.get('/categories/:category', async (req, res) => res.send(await new PageCategoryInner(req).render()));
