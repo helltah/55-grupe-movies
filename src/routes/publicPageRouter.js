4cef74fbcb88d1bde9f4df93adf516204fdcd98c
Vitalij <203612403+helltah@users.noreply.github.com>
2025-07-28 10:49:59 +0300

    category inner

--- src/routes/publicPageRouter.js
+++ src/routes/publicPageRouter.js
@@ -1,11 +1,10 @@
-
 import express from 'express';
 import { PageHome } from '../pages/public/Home.js';
 import { PageMovies } from '../pages/public/Movies.js';
-
+import { PageCategories } from '../pages/public/Categories.js';
+import { PageCategoryInner } from '../pages/public/CategoryInner.js';
 import { PageLogin } from '../pages/public/Login.js';
 import { PageRegister } from '../pages/public/Register.js';
-import { PageCategories } from '../pages/public/Categories.js';
 
 export const publicPageRouter = express.Router();
 
@@ -14,6 +13,7 @@ publicPageRouter.get('/', async (req, res) => res.send(await new PageHome(req).r
 publicPageRouter.get('/movies', async (req, res) => res.send(await new PageMovies(req).render()));
 
 publicPageRouter.get('/categories', async (req, res) => res.send(await new PageCategories(req).render()));
+publicPageRouter.get('/categories/:category', async (req, res) => res.send(await new PageCategoryInner(req).render()));
 
 publicPageRouter.get('/login', async (req, res) => res.send(await new PageLogin(req).render()));
 publicPageRouter.get('/register', async (req, res) => res.send(await new PageRegister(req).render()));
